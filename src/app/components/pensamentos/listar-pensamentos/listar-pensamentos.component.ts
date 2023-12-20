d6a93cf4ba1f4e392f8f6658413854ce0abd8425
LucasMartines01 <lucas.martines@sptech.school>
2023-12-20 13:57:27 -0300

    Created component to list thoughts

---
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'XXXXXXX',
+      autoria: 'Nay',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Teste',
+      autoria: 'Teste',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+      autoria: 'Teste',
+      modelo: 'modelo2'
+    },
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
